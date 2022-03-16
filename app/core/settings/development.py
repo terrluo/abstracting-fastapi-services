e2caa9cb2248faa9263f6ff7144b005e710581df
xintao_luo <xintao_luo@fzzixun.com>
2022-03-16 11:53:21 +0800

    add config

---
+++ app/core/settings/development.py
@@ -0,0 +1,14 @@
+import logging
+
+from app.core.settings.app import AppSettings
+
+
+class DevAppSettings(AppSettings):
+    debug: bool = True
+
+    title: str = "Dev Fastapi Service"
+
+    logging_level: int = logging.DEBUG
+
+    class Config(AppSettings.Config):
+        env_file = ".env"
